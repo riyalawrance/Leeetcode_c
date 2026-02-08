e2a094efe97561778378577322da786aa39a040f
Riya Lawrance <148250797+riyalawrance@users.noreply.github.com>
2026-02-08 15:05:41 +0530

    Implement function to reverse words in a string

--- leetcode/reverse_words_in_a_string.c
+++ leetcode/reverse_words_in_a_string.c
@@ -1,1 +1,35 @@
-
+#include <string.h>
+char* reverseWords(char* s) 
+{
+    int len=strlen(s),ch=0,k=0;
+    char* result = (char*)malloc(sizeof(char)*len+1);
+    char* word = (char*)malloc(sizeof(char)*len+1);
+    word[0] = '\0';
+    for(int i=len-1;i>=0;i--)
+    {
+        if(s[i]!=' ')
+        {
+            word[k]=s[i];
+            k++;
+        }
+        if(s[i]==' '||i==0)
+        {
+            word[k]='\0';
+            int l=strlen(word);
+            for(int j=0;word[j]!='\0';j++)
+            {
+                result[ch]=word[l-j-1];
+                //result[ch++]=" ";
+                ch++;
+            }
+            k=0;
+            if(i!=0 && s[i-1]!=' '&&ch!=0)
+            {
+                result[ch]=s[i];
+                ch++;
+            }
+        }
+    }
+    result[ch]='\0';
+    return result;
+}
